b022f1eeee909baf25110375a84b2659e93e64f8
Bintang Timurlangit <btimurlangit@gmail.com>
2025-08-07 17:13:44 +0700

    feat: Add allowedDevOrigins to Next.js configuration for local development
    - Introduce a list of allowed development origins to enhance security and facilitate local testing.
    - Include common local addresses for easier access during development.

--- next.config.mjs
+++ next.config.mjs
@@ -3,6 +3,13 @@ import { withPayload } from '@payloadcms/next/withPayload'
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   // Your Next.js config here
+  allowedDevOrigins: [
+    'http://localhost:3000',
+    'http://127.0.0.1:3000',
+    'http://192.168.1.1:3000',
+    'http://192.168.0.1:3000',
+    'http://192.168.31.254:3000',
+  ],
   webpack: (webpackConfig) => {
     webpackConfig.resolve.extensionAlias = {
       '.cjs': ['.cts', '.cjs'],
