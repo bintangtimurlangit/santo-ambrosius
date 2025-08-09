65f2569dcce854aef53b4943281391eeaf8fdff1
Bintang Timurlangit <btimurlangit@gmail.com>
2025-08-09 11:25:57 +0700

    feat: Enhance metadata and icon configuration in layout
    - Added website description and manifest to the metadata for improved SEO and PWA support.
    - Included icon configurations for various sizes and types to enhance branding and user experience.

--- src/app/(frontend)/layout.tsx
+++ src/app/(frontend)/layout.tsx
@@ -4,8 +4,17 @@ import Navbar from '@/components/Navbar'
 import Footer from '@/components/Footer'
 
 export const metadata = {
-  description: 'Website resmi Gereja Santo Ambrosius',
   title: 'Gereja Santo Ambrosius',
+  description: 'Website resmi Gereja Santo Ambrosius',
+  manifest: '/site.webmanifest',
+  icons: {
+    icon: [
+      { url: '/favicon-16x16.png', sizes: '16x16', type: 'image/png' },
+      { url: '/favicon-32x32.png', sizes: '32x32', type: 'image/png' },
+      { url: '/favicon.ico', sizes: 'any' },
+    ],
+    apple: [{ url: '/apple-touch-icon.png', sizes: '180x180', type: 'image/png' }],
+  },
 }
 
 export default async function RootLayout(props: { children: React.ReactNode }) {
